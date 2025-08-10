159d705f760f7780653e44fe840d1a7971e70cf6
Jose Acevedo <joseacevedo0501@gmail.com>
2025-08-10 22:05:28 +0200

    incluir forms.py

---
+++ app/forms.py
@@ -0,0 +1,21 @@
+# app/forms.py
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_ckeditor import CKEditorField
+
+class PostForm(FlaskForm):
+    titulo      = StringField('Título', validators=[DataRequired()])
+    descripcion = StringField('Descripción breve', validators=[DataRequired()])
+    img_url     = StringField('URL de la imagen')
+    img_fuente  = StringField('Fuente de la imagen')
+    tag         = StringField('Etiqueta principal')
+    autor       = StringField('Autor', validators=[DataRequired()])
+    contenido   = CKEditorField('Contenido', validators=[DataRequired()])
+    submit      = SubmitField('Publicar')
+
+class CommentForm(FlaskForm):
+    nombre = StringField('Nombre', validators=[DataRequired()])
+    correo = StringField('Correo electrónico', validators=[DataRequired()])
+    comentario = StringField('Comentario', validators=[DataRequired()])
+    submit = SubmitField('Enviar comentario')
